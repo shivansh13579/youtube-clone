422bc9df541049c3887cb71a4e97565d66bfa048
shivansh13579 <shivansh0975@gmail.com>
2023-12-25 10:56:56 +0530

    learn middleware and mongoose connection

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!! ",err);
+})
+
+
+
+
+
+
+
+
+
+
+
+/*
+( async () => {
+     try {
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+     } catch (error) {
+        console.error("ERROR: ",error)
+        throw err
+     }
+})()
+*/
