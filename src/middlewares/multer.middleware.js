5c9eae3a44cd07a89d0311f0a721757d0f40ea2a
shivansh13579 <shivansh0975@gmail.com>
2023-12-27 21:51:25 +0530

    create user controler

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -5,9 +5,10 @@ const storage = multer.diskStorage({
       cb(null, "./public/temp")
     },
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
+      cb(null, file.originalname)
     }
   })
   
-  const upload = multer({ storage: storage })
+export const upload = multer({
+    storage,
+  })
